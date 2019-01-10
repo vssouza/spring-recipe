9f49e0007d587c367ffc667c98ed2f168a2b0367
Vinicius Yamauchi <vs.souza@gmail.com>
2019-01-10 15:55:04 +0000

    project structure

---
+++ src/main/java/com/example/spring/recipe/SpringRecipeApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeApplication.class, args);
+    }
+
+}
+
